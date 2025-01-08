9a797306067e9dcc2a73fd1dd8c07ff03bca58b4
BozhidarYordanov54 <bozhidar.yordanov02@gmail.com>
2025-01-08 12:56:44 +0200

    first commit

---
+++ 01_SumMatrixElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine()
+                                    .Split(", ")
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            int sumOfElements = 0;
+
+            for (int column = 0; column < matrix.GetLength(0); column++)
+            {
+                int[] matrixValue = Console.ReadLine()
+                                    .Split(", ")
+                                    .Select(int.Parse)
+                                    .ToArray();
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    int currentValue = matrixValue[row];
+                    matrix[column, row] = currentValue;
+                    sumOfElements += currentValue;
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sumOfElements);
+        }
+    }
+}
