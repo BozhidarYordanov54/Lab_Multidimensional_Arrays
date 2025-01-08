be15e50825d15a7ff2b67cedd16c696212ebd409
BozhidarYordanov54 <bozhidar.yordanov02@gmail.com>
2025-01-08 13:31:12 +0200

    Added new exercise solution

--- 04_SymbolInMatrix/Program.cs
+++ 04_SymbolInMatrix/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+char[,] matrix = fillMatrix(n);
+
+char symbolToSearch = char.Parse(Console.ReadLine());
+
+string result = searchArray(matrix, symbolToSearch);
+
+Console.WriteLine(result);
+
+char[,] fillMatrix(int n)
+{
+    char[,] result = new char[n, n];
+
+    for (int row = 0; row < n; row++)
+    {
+        string? values = Console.ReadLine();        
+        for (int col = 0; col < n; col++)
+        {
+            result[row, col] = values[col]; 
+        }
+    }
+
+    return result;
+}
+
+string searchArray(char[,] arr, char symbolToSearch)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if(arr[i,j] == symbolToSearch)
+            {
+                return $"({i}, {j})";
+            }
+        }
+    }
+
+    return $"{symbolToSearch} does not occur in the matrix";
+}
